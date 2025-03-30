af14e9b8adc8f8194f08ee1e7c2364aec56636a0
olsonadr <olsonnick10@yahoo.com>
2025-03-30 17:33:48 +0200

    Add initial question_1 solution

---
+++ src/question_1.py
@@ -0,0 +1,40 @@
+"""Solution for Question 1: Adjacency Matrix to Adjacency List"""
+
+def mat_to_list(adj_mat):
+    """ Convert adjacency matrix to an adjacency list representation
+
+    Parameters:
+    -----------
+    adj_mat : (a square 0-1 matrix)
+        Adjacency matrix (n x n) of the graph (of n nodes)
+
+    Returns:
+    --------
+    adj_list: `list[list[int]]`
+        The adjacency list of the graph
+
+    1 - Direct approach:
+    ----------------------------
+    Steps:
+     - Iterate through rows of matrix, iterate through each row:
+       - If an element is not 0, append it's index in the row to the output row
+    Notes:
+     - Clearly paralelizable for every row as no prev row impacts current row behavior
+     - Can't ignore half of the table because edges are directed
+    """
+
+    # # 1 - Direct approach (using loops):
+    # adj_list = []
+    # for row in adj_mat:
+    #     adj_list_row = []
+    #     for i, val in enumerate(row):
+    #         if val != 0:
+    #             adj_list_row.append(i)
+    #     adj_list.append(adj_list_row)
+    # return adj_list
+
+    # 1 - Direct approach (using list comprehension):
+    return [
+        [ i for i, val in enumerate(row) if val != 0 ]
+        for row in adj_mat
+    ]
